e0338887c71de0fe55b37cc14b950766e950e090
Hari Narayanan <lnarayanan34@gmail.com>
2019-01-19 15:05:28 +0530

    Added redirecting urls

--- api/urls.py
+++ api/urls.py
@@ -6,5 +6,11 @@ from django.shortcuts import redirect
 app_name = 'api'
 
 urlpatterns = [
+        path('guide/info/<decimal:lat>/<decimal:long>/', views.guideinfo, name='guide-info'),
+        path('place/additionalinfo/<decimal:lat>/<decimal:long>/',views.additionalinfo, name='place-info'),
+        path('bookingservices/<decimal:lat>/<decimal:long>/',view.bookservices, name='booking-services'),
+        path('parkinglot/details/<decimal:lat>/<decimal:long>/',views.parkinginfo, name='parking-info'),
+        path('place/police-details/<decimal:lat>/<decimal:long>/',views.policedetails, name='police-details'),
+        path('place/guide-details/<decimal:lat>/<decimal:long>/',views.guidedetails, name='police-details')
 
 ]
