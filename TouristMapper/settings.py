dfa3606dbe52a1f99be34600857b36a7691d4b86
BharathKumarRavichandran <bharathkumarravichandran@gmail.com>
2019-01-20 01:28:53 +0530

    Add CORS middleware to settings.py

--- TouristMapper/settings.py
+++ TouristMapper/settings.py
@@ -30,6 +30,13 @@ ALLOWED_HOSTS = [
     '127.0.0.1'
 ]
 
+# CORS rules
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = (
+    'localhost:1234',
+    '*'
+    )
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
 
@@ -41,11 +48,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api.apps.ApiConfig',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
