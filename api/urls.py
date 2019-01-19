fd8ef8eea24101bd67f86a87c116a3ef1597588f
BharathKumarRavichandran <bharathkumarravichandran@gmail.com>
2019-01-19 10:18:43 +0530

    Add api.urls

---
+++ api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from django.conf.urls import url
+from django.views.generic import RedirectView
+from django.shortcuts import redirect
+
+app_name = 'api'
+
+urlpatterns = [
+
+]
