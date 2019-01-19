63d7648126fe95a79530d62b372bf769474eee40
BharathKumarRavichandran <bharathkumarravichandran@gmail.com>
2019-01-19 19:13:08 +0530

    Update api.urls

--- api/urls.py
+++ api/urls.py
@@ -2,15 +2,19 @@ from django.urls import path, include
 from django.conf.urls import url
 from django.views.generic import RedirectView
 from django.shortcuts import redirect
+from . import views
 
 app_name = 'api'
 
 urlpatterns = [
-        path('guide/info/<decimal:lat>/<decimal:long>/', views.guideinfo, name='guide-info'),
-        path('place/additionalinfo/<decimal:lat>/<decimal:long>/',views.additionalinfo, name='place-info'),
-        path('bookingservices/<decimal:lat>/<decimal:long>/',view.bookservices, name='booking-services'),
-        path('parkinglot/details/<decimal:lat>/<decimal:long>/',views.parkinginfo, name='parking-info'),
-        path('place/police-details/<decimal:lat>/<decimal:long>/',views.policedetails, name='police-details'),
-        path('place/guide-details/<decimal:lat>/<decimal:long>/',views.guidedetails, name='police-details')
+        path('guide/info/', views.GuideInfoView.as_view() , name='guide-info'),
+
+        path('place/additionalinfo/', views.PlaceAdditionalView.as_view(), name='place-info'),
+        
+        path('bookingservices/', views.BookingServicesView.as_view(), name='booking-services'),
+        
+        path('parkinglot/details/', views.ParkingInfoView.as_view(), name='parking-info'),
+        
+        path('place/police-details/', views.PoliceDetailsView.as_view(), name='police-details'),
 
 ]
